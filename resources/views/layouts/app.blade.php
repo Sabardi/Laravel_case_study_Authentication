a67265e76511ccc9ab15427f33d22a4116f10df5
Sabardi <Sabardibahari@gmail.com>
2024-05-10 18:53:23 +0800

    update insert dellet finish

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/')}}">Tabel Jurusan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/jurusans/create')}}">Tambah Jurusan</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
