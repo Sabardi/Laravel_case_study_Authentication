014869c673d78405e4056b72afdebc5bb51f2277
Sabardi <Sabardibahari@gmail.com>
2024-05-10 18:33:46 +0800

    add controller, routes model dan view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\JurusanController;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +21,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', [JurusanController::class,'index'])->middleware('auth');
+Route::resource('jurusans',JurusanController::class)->middleware('auth');
